23c40e368afe8154316bb94680bdcf47cc1537dd
Lou <phucknguyen@gmail.com>
2016-03-23 20:41:22 +0700

    Add initial support for command line

---
+++ src/main/java/lou/arane/AraneManual.java
@@ -0,0 +1,14 @@
+package lou.arane;
+
+/**
+ * Test the main class.
+ * Not a unit test since it needs to hit the cloud.
+ * 
+ * @author Phuc
+ */
+public class AraneManual {
+
+	public static void main(String[] args) {
+		Arane.main(new String[] {"http://manga.life/read-online/Berserk"});
+	}
+}
